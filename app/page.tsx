7e58e7945dffd0dc6b98b83305a7ab68fc23c108
Hsueh-Ning Chao <125740431+hsuehningchao@users.noreply.github.com>
2025-05-23 23:17:37 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -137,8 +137,9 @@ export default function Personal() {
       >
         <div className="flex-1">
           <p className="text-zinc-600 dark:text-zinc-400">
-            Focused on creating intuitive and performant web experiences.
-            Bridging the gap between design and development.
+            Project Manager passionate about supply chain, hands-on experience in logistics optimization utilizing data analytics with SQL, Python.
+            2.5 years of experience bridging public-sector operations and data-driven decision-making. Skilled in
+            requirements gathering, sourcing strategy, and stakeholder alignment. 
           </p>
         </div>
       </motion.section>
