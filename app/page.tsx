aa811777e5664c080700ebadfd63aa8d2e92b911
Hsueh-Ning Chao <125740431+hsuehningchao@users.noreply.github.com>
2025-05-24 15:28:02 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -203,13 +203,18 @@ export default function Personal() {
               />
               <div className="relative h-full w-full rounded-[15px] bg-white p-4 dark:bg-zinc-950">
                 <div className="relative flex w-full flex-row justify-between">
-                  <div>
-                    <h4 className="font-normal dark:text-zinc-100">
-                      {job.title}
-                    </h4>
-                    <p className="text-zinc-500 dark:text-zinc-400">
-                      {job.company}
-                    </p>
+                  <div className="flex items-start space-x-3">
+                    {job.logo && (
+                      <img
+                        src={job.logo}
+                        alt={`${job.company} logo`}
+                        className="h-8 w-8 object-contain rounded-md"
+                      />
+                    )}
+                    <div>
+                      <h4 className="font-normal dark:text-zinc-100">{job.title}</h4>
+                      <p className="text-zinc-500 dark:text-zinc-400">{job.company}</p>
+                    </div>
                   </div>
                   <p className="text-zinc-600 dark:text-zinc-400">
                     {job.start} - {job.end}
